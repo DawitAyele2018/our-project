433434da3abc7f1bd3ebe9ec65789560d26b57d6
DawitAyele2018 <dayele@mail.sfsu.edu>
2019-03-06 16:58:25 -0800

    second push

--- app.js
+++ app.js
@@ -4,15 +4,17 @@ const request = require("request");
 
 const app = express();
 var response = {};
+var stat = true;
 
 app.use(bodyParser.urlencoded({extended:true}));
 
 app.get("/", function(req,res){
   res.sendFile(__dirname + "/index.html");
 });
-
+console.log("hello");
 // app.post("/",function(req,res) {
-//
+//   //alert(req.body.sport);
+//   //if(req.body.sport !== null){
 //   var team = req.body.sport;
 //   var baseURL= "https://www.thesportsdb.com/api/v1/json/1/search_all_leagues.php?c=USA&s=";
 //   var key = "&APPID=4013053";
@@ -32,15 +34,18 @@ app.get("/", function(req,res){
 //
 //
 //   });
+// //  }
 //
 // });
 
 app.post("/",function(req,res) {
+  if(req.body.city == "Hello"){
   var city = req.body.city;
   var baseURL= "http://api.openweathermap.org/data/2.5/weather?q=";
   var key = "&APPID=218f955661efb740eb8338b5409903c2";
   var finalURL = baseURL + city + key;
   request(finalURL,function(error,response,body){
+
     var data = JSON.parse(body);
 
                 response = {
@@ -49,13 +54,15 @@ app.post("/",function(req,res) {
                         "city" : req.body.city,
                     },
                     "response": data.main,
-                    "description": "returns an object that coming from Canadeian football league"
+                    "description": "returns the current weather of the city"
   };
     res.send(response)
 
 
 
   });
+
+}
 
 });
 
